60a0215f10ce8efca4da6311eac54df3ff068e0b
OKrylov <krylov92oa@gmail.com>
2019-03-07 23:50:07 +0300

    Lesson 3

---
+++ Lesson3_Streams/src/objects/stream/Human.java
@@ -0,0 +1,18 @@
+package objects.stream;
+
+import java.io.Serializable;
+
+public class Human implements Serializable {
+    static final long serialVersionUID = -1L;
+
+
+    public int age;
+
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "age=" + age +
+                '}';
+    }
+}
